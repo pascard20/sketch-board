5f537c350ebda322cb6b04c868be987a54c00126
pascard20 <141923751+pascard20@users.noreply.github.com>
2023-10-24 18:52:21 +0200

    Create initial working version of the app

---
+++ script.js
@@ -0,0 +1,40 @@
+elemGridWrapper = document.querySelector('.grid__wrapper');
+elemBtnReset = document.querySelector('.btn--reset');
+elemBtnBoard = document.querySelector('.btn--board');
+elemRoot = document.documentElement;
+
+const DEFAULT_ITEM_COUNT = 16;
+const MAX_ITEM_COUNT = 100;
+
+const createBoard = itemCount => {
+    elemRoot.style.setProperty('--gridItemCount', itemCount);
+    elemGridWrapper.innerHTML = '';
+    for (let i = 0; i < itemCount ** 2; i++) {
+        elemGridWrapper.insertAdjacentHTML('beforeend', `
+    <div class="grid__item"></div>
+    `)
+    }
+}
+
+const handleHover = event => {
+    elemCurrent = event.target;
+    if (elemCurrent.className.includes('wrapper')) return;
+    elemCurrent.classList.add('active');
+}
+
+const handleReset = () => {
+    [...elemGridWrapper.children].forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+const handleNewBoard = () => {
+    const newItemCount = prompt(`Enter number of columns/rows (max ${MAX_ITEM_COUNT}):`)
+    if (newItemCount > 0 && newItemCount <= MAX_ITEM_COUNT) createBoard(newItemCount);
+}
+
+elemGridWrapper.addEventListener('mouseover', handleHover);
+elemBtnReset.addEventListener('click', handleReset)
+elemBtnBoard.addEventListener('click', handleNewBoard)
+
+createBoard(DEFAULT_ITEM_COUNT);
